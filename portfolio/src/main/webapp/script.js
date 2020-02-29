0138550782994fe91c46d5d2afea5c08df51eb4e
anooj-shah <thisisanooj@gmail.com>
2020-02-28 18:48:30 -0600

    Remove excess comments

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,28 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
-*/
 function addRandomGreeting() {
   const greetings =
       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
 
-/**
- * New Section for Greeting - Part 2
- */
-
-/**
- * Fetches greeting from the server and adds it to the DOM.
- */
 function getGreeting() {
   console.log('Fetching greeting.');
 
@@ -44,23 +30,13 @@ function getGreeting() {
   responsePromise.then(handleResponse);
 }
 
-/**
- * Handles response by converting it to text and passing the result to
- * addGreetingToDom().
- */
 function handleResponse(response) {
   console.log('Handling the response.');
-
-  // response.text() returns a Promise, because the response is a stream of
-  // content and not a simple variable.
+  // response.text() returns a Promise, because the response is a stream of content and not a simple variable.
   const textPromise = response.text();
-
-  // When the response is converted to text, pass the result into the
-  // addGreetingToDom() function.
   textPromise.then(addGreetingToDom);
 }
 
-/** Adds greeting to the DOM. */
 function addGreetingToDom(greeting) {
   console.log('Adding greeting to dom: ' + greeting);
   const greetingContainer = document.getElementById('greeting-container');
