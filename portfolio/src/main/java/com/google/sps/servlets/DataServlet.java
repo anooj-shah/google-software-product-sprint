b44f656c2beead8e17b1df7c89280905afd74090
anooj-shah <thisisanooj@gmail.com>
2020-03-04 16:56:59 -0600

    Sentiment Analysis

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,6 +14,9 @@
 
 package com.google.sps.servlets;
 
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.Sentiment;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -66,12 +69,13 @@ public class DataServlet extends HttpServlet {
     LanguageServiceClient languageService = LanguageServiceClient.create();
     Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
     float score = sentiment.getScore();
-    System.out.println(score)
+    System.out.println("Score: " + score);
     languageService.close();
 
     long timestamp = System.currentTimeMillis();
 
     Entity messageEntity = new Entity("Message");
+    messageEntity.setProperty("score", score);
     messageEntity.setProperty("message", message);
     messageEntity.setProperty("timestamp", timestamp);
 
