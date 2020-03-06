3b8b23d2f2908347f8ef4842a38477d64c6b2045
anooj-shah <thisisanooj@gmail.com>
2020-03-06 14:12:17 -0600

    Fix score

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -75,12 +75,12 @@ public class DataServlet extends HttpServlet {
     LanguageServiceClient languageService = LanguageServiceClient.create();
     Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
     double score = sentiment.getScore();
-    System.out.println("Score: " + score);
     languageService.close();
 
     long timestamp = System.currentTimeMillis();
 
     Entity messageEntity = new Entity("Message");
+    messageEntity.setProperty("score", score);
     messageEntity.setProperty("message", message);
     messageEntity.setProperty("timestamp", timestamp);
 
